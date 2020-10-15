8f8754be224ffaf2ee847ec460d79025348fe8f4
Ans-Kumar <anshu.1000kumar@gmail.com>
2020-10-16 02:35:37 +0530

    Initial Commit

---
+++ emailproject/demoapp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
+# Create your views here.
+def sendanemail(request):
+    if request.method == "POST":
+        to = request.POST.get('toemail')
+        content = request.POST.get('content')
+        #print(to,content)
+        send_mail(
+            #subject
+            "testing",
+            #msg
+            content,
+            #from email
+            settings.EMAIL_HOST_USER,
+            #rec list
+            [to]
+        )
+        return render(
+            request,
+            'email.html',
+            {
+                'title': 'send an email'
+            }
+        )
+    else:
+        return render(
+            request,
+            'email.html',
+            {
+                'title': 'send an email'
+            }
+        )
